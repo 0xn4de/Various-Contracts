59577210a82eba4a471d228e5966dafbd5ccfc07
0xn4de <109149873+0xn4de@users.noreply.github.com>
2024-01-17 22:25:28 +0200

    Improve readability

--- test/Jan06_ERC1155Vault.t.sol
+++ test/Jan06_ERC1155Vault.t.sol
@@ -10,6 +10,7 @@ contract ERC1155VaultTest is Test {
     MockERC1155 underlying2;
     MockERC1155 underlying3;
     MockERC1155Vault vault;
+
     address alice;
     address bob;
 
@@ -18,27 +19,36 @@ contract ERC1155VaultTest is Test {
         underlying2 = new MockERC1155();
         underlying3 = new MockERC1155();
         vault = new MockERC1155Vault();
+
         alice = makeAddr("alice");
         bob = makeAddr("bob");
+
         vault.create(underlying, 1);
         vault.create(underlying2, 1);
         vault.create(underlying3, 1);
         vault.create(underlying3, 2); // create two vaults with underlying3, one for tokenId 1 and one for 2
+        vm.startPrank(alice);
+        underlying.setApprovalForAll(address(vault), true);
+        vm.startPrank(bob);
+        underlying.setApprovalForAll(address(vault), true);
+        underlying2.setApprovalForAll(address(vault), true);
+        underlying3.setApprovalForAll(address(vault), true);
+        vm.stopPrank();
     }
     function testDepositToVault() public {
         // mint bob 5 erc1155s with tokenId 1
         underlying.mint(bob, 1, 5, "");
+
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
         vault.deposit(underlying, 1, 5, bob);
         vm.stopPrank();
+
         assertEq(vault.balanceOf(bob, 1), 5);
     }
     function testMintFromVault() public {
         // mint bob 5 erc1155s with tokenId 1
         underlying.mint(bob, 1, 5, "");
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
         vault.mint(underlying, 1, 5, bob);
         vm.stopPrank();
         assertEq(vault.balanceOf(bob, 1), 5); // Check bob's balance for vault #1 tokens
@@ -47,7 +57,6 @@ contract ERC1155VaultTest is Test {
         // mint bob 5 erc1155s with tokenId 1
         underlying.mint(bob, 1, 5, "");
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
         vault.mint(underlying, 1, 5, bob);
         assertEq(vault.balanceOf(bob, 1), 5);
         // withdraw his tokens back
@@ -58,7 +67,6 @@ contract ERC1155VaultTest is Test {
         // mint bob 5 erc1155s with tokenId 1
         underlying.mint(bob, 1, 5, "");
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
         vault.mint(underlying, 1, 5, bob);
         assertEq(vault.balanceOf(bob, 1), 5);
         // withdraw his tokens back
@@ -70,25 +78,28 @@ contract ERC1155VaultTest is Test {
         underlying2.mint(bob, 1, 4, "");
         underlying3.mint(bob, 1, 3, "");
         underlying3.mint(bob, 2, 10, "");
+
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
-        underlying2.setApprovalForAll(address(vault), true);
-        underlying3.setApprovalForAll(address(vault), true);
+
         vault.mint(underlying, 1, 5, bob);
         vault.mint(underlying2, 1, 4, bob);
         vault.mint(underlying3, 1, 3, bob);
         vault.mint(underlying3, 2, 10, bob);
+
         assertEq(underlying.balanceOf(bob, 1), 0); // check that something did go out
+
         // check that bob got his various ERC1155 tokens with different tokenIds
         assertEq(vault.balanceOf(bob, 1), 5);
         assertEq(vault.balanceOf(bob, 2), 4);
         assertEq(vault.balanceOf(bob, 3), 3);
         assertEq(vault.balanceOf(bob, 4), 10);
+    
         // start withdrawing, 2 redeems 2 withdraws for good measure
         vault.redeem(underlying, 1, 5, bob, bob, ""); 
         vault.withdraw(underlying2, 1, 4, bob, bob, ""); 
         vault.redeem(underlying3, 1, 3, bob, bob, ""); 
         vault.withdraw(underlying3, 2, 10, bob, bob, ""); 
+
         assertEq(underlying.balanceOf(bob, 1), 5);
         assertEq(underlying2.balanceOf(bob, 1), 4);
         assertEq(underlying3.balanceOf(bob, 1), 3);
@@ -98,20 +109,23 @@ contract ERC1155VaultTest is Test {
         // mint bob and alice 5 erc1155s with tokenId 1
         underlying.mint(bob, 1, 5, "");
         underlying.mint(alice, 1, 5, "");
+
         vm.startPrank(bob);
-        underlying.setApprovalForAll(address(vault), true);
         vault.mint(underlying, 1, 5, bob);
         assertEq(vault.balanceOf(bob, 1), 5);
+
         vm.stopPrank();
+
         vm.startPrank(alice);
-        underlying.setApprovalForAll(address(vault), true);
         vault.mint(underlying, 1, 5, alice);
         vm.stopPrank();
+
         vm.startPrank(bob);
         // withdraw his tokens back
         vault.redeem(underlying, 1, 5, bob, bob, ""); 
         assertEq(underlying.balanceOf(bob, 1), 5); // check that bob got his tokens back
         vm.stopPrank();
+
         vm.startPrank(alice);
         vault.balanceOf(alice, 1);
         vault.redeem(underlying, 1, 5, alice, alice, ""); 
