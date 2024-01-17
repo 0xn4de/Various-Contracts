c24c7cf9df5e9de441a590572cd597d592a10a0c
0xn4de <109149873+0xn4de@users.noreply.github.com>
2024-01-17 22:18:51 +0200

    Cleanup

--- test/Jan01_WannaBet.t.sol
+++ test/Jan01_WannaBet.t.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import "forge-std/console.sol";
-import {WannaBet, Side, BetData, AggregatorV3Interface} from "../src/Jan01_WannaBet.sol";
+import {WannaBet, Side} from "../src/Jan01_WannaBet.sol";
 contract WannaBetTest is Test {
     WannaBet public betContract;
-    // AggregatorV3Interface priceFeed;
     address alice;
     address bob;
     address charlie;
     function setUp() public {
         vm.createSelectFork(vm.rpcUrl('mainnet'), 18800000); // ETH Price 2254.51
         betContract = new WannaBet(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-        // priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+
         alice = makeAddr("alice");
         bob = makeAddr("bob");
         charlie = makeAddr("charlie");
+
         vm.deal(alice, 10 ether);
         vm.deal(bob, 10 ether);
         vm.deal(charlie, 10 ether);
@@ -114,6 +114,7 @@ contract WannaBetTest is Test {
         // Bob bets that 1 ETH that ETH is over 2280 USDC in 5 days, with a maximum time to accept bet of 2 days, settles in 5 days
         uint256 betId = betContract.createBet{value: 1 ether}(2280*1e8, Side.OVER, block.timestamp + 5 days, block.timestamp + 2 days, 1 ether);
         vm.warp(block.timestamp + 2.01 days);
+        assertEq(address(bob).balance, 9 ether);
         betContract.closeBet(betId);
         assertEq(address(bob).balance, 10 ether); // Funds get returned
 
