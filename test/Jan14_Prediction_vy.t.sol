783403f2f7b7a43c398f5f7ed82db40dee08efa1
0xn4de <109149873+0xn4de@users.noreply.github.com>
2024-01-18 23:01:57 +0200

    Add vyper version of Prediction

---
+++ test/Jan14_Prediction_vy.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {Prediction} from "../src/Jan14_Prediction.sol";
+import "vyper/VyperDeployer.sol";
+
+contract PredictionTest is Test {
+    address alice;
+    address bob;
+
+    VyperDeployer vyperDeployer = new VyperDeployer();
+    
+    Prediction private prediction;
+
+    function setUp() public {
+        prediction = Prediction(vyperDeployer.deployContract("Jan14_Prediction"));
+
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
+    }
+
+    function testPredict() public {
+        vm.prank(bob);
+        uint256 predId = prediction.createPrediction(keccak256(bytes("hello")));
+        bool revealed = prediction.revealPrediction(predId, "hello"); // revealable by anyone, this isnt being called by bob
+        assert(revealed);
+    }
+    function testPredictFail() public {
+        vm.prank(bob);
+        uint256 predId = prediction.createPrediction(keccak256(bytes("hello")));
+
+        vm.expectRevert("Empty prediction");
+        prediction.revealPrediction(predId, "");
+
+        vm.expectRevert("Wrong reveal");
+        prediction.revealPrediction(predId, "abcde");
+
+        bool revealed = prediction.revealPrediction(predId, "hello");
+        assert(revealed);
+        
+        vm.expectRevert("Prediction already set");
+        prediction.revealPrediction(predId, "hello");
+    }
+}
---
+++ vyper_contracts/Jan14_Prediction.vy
@@ -0,0 +1,27 @@
+
+
+predId: public(uint256)
+
+struct Pred:
+    isRevealed: bool
+    maker: address
+    hash: bytes32
+    pred: String[1000]
+
+predictions: public(HashMap[uint256, Pred])
+
+
+@external
+def createPrediction(_hash: bytes32) -> uint256:
+    self.predId += 1
+    self.predictions[self.predId] = Pred({isRevealed: False, maker: msg.sender, hash: _hash, pred: ""})
+    return self.predId
+
+@external
+def revealPrediction(_predId: uint256, prediction: String[100]) -> bool:
+    p: Pred = self.predictions[_predId]
+    assert p.pred == "", "Prediction already set"
+    assert prediction != "", "Empty prediction"
+    assert p.hash == keccak256(prediction), "Wrong reveal"
+    self.predictions[_predId].pred = prediction # p.pred = prediction doesn't change var? curious
+    return True
